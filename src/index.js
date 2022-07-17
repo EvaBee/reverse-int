f51d4a5faa87b50467cc7b7e6094fbb96b29a409
Eva <bebinaeva@gmail.com>
2022-07-17 21:49:12 +0300

    feat: implement function that reverse digits of this number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+   if(n < 0){
+       n = Math.abs(n);
+   }
+    return n.toString().split('').reverse().join('') * Math.sign(n);
 }
+/*
+
+z = reverse(-95034);
+console.log(z)
+*/
